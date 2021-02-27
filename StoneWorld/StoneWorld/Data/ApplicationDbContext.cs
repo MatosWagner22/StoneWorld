a9b1c4b3cc4d80ef7cbf87aab2e2236f9f935583
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2021-02-27 19:56:38 -0400

    ConnectionString + Packeges + DbContext

---
+++ StoneWorld/StoneWorld/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoneWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoneWorld.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+
+    }
+}
