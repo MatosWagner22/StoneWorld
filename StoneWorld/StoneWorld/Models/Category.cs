576a93807b8466a71d4af9f5bfb3a1bdd90860ce
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2021-03-01 16:04:32 -0400

    Create

--- StoneWorld/StoneWorld/Models/Category.cs
+++ StoneWorld/StoneWorld/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,7 +13,8 @@ namespace StoneWorld.Models
         public int Id { get; set; }
 
         //[Required]
-        public String Name { get; set; }
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
