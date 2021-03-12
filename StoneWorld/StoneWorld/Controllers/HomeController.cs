f25d295a85a86cdb570a2ba7c41221c6361949f8
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2021-03-11 23:19:48 -0400

    Product Details

--- StoneWorld/StoneWorld/Controllers/HomeController.cs
+++ StoneWorld/StoneWorld/Controllers/HomeController.cs
@@ -34,6 +34,18 @@ namespace StoneWorld.Controllers
             return View(homeVM);
         }
 
+        public IActionResult Details(int id)
+        {
+            DetailsVM DetailsVM = new DetailsVM()
+            {
+                Product = _db.Product.Include(u => u.Category).Include(u => u.ApplicationType).
+                Where(u => u.Id == id).FirstOrDefault(),
+                ExistsInCart = false
+            };
+
+            return View(DetailsVM);
+        }
+
         public IActionResult Privacy()
         {
             return View();
---
+++ StoneWorld/StoneWorld/Models/ViewModels/DetailsVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoneWorld.Models.ViewModels
+{
+    public class DetailsVM
+    {
+        public DetailsVM()
+        {
+            Product = new Product();//Singleton
+        }
+
+        public Product Product { get; set; }
+        public bool ExistsInCart { get; set; }
+    }
+}
