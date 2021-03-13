3d3b8b94f71876607c61cab8b6f2514f52d19470
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2021-03-12 20:39:27 -0400

    Scaffolding Identity tables v.2

---
+++ StoneWorld/StoneWorld/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StoneWorld.Data;
+
+[assembly: HostingStartup(typeof(StoneWorld.Areas.Identity.IdentityHostingStartup))]
+namespace StoneWorld.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
