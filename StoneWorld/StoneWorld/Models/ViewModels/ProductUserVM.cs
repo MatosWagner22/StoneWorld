5f4f0d8f2c06218d30ad9a39056c25fe14378eb9
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2021-03-22 18:12:31 -0400

    Cart + Summary

---
+++ StoneWorld/StoneWorld/Models/ViewModels/ProductUserVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoneWorld.Models.ViewModels
+{
+    public class ProductUserVM
+    {
+        public ProductUserVM()
+        {
+            ProductList = new List<Product>(); // singleton
+        }
+
+        public ApplicationUser ApplicationUser  { get; set; }
+        public IEnumerable<Product> ProductList { get; set; }
+    }
+}
