163f45bb4cdd6cc6b86dd841fe0c715116352276
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2021-05-15 21:21:25 -0400

    Order Confirmation

--- StoneWorld/StoneWorld/Controllers/CartController.cs
+++ StoneWorld/StoneWorld/Controllers/CartController.cs
@@ -267,11 +267,13 @@ namespace StoneWorld.Controllers
             return RedirectToAction(nameof(InquiryConfirmation));
         }
 
-        public IActionResult InquiryConfirmation()
+        public IActionResult InquiryConfirmation(int id = 0)
         {
+            OrderHeader orderHeader = _orderHRepo.FirstOrDefault(u => u.Id == id);
+
             HttpContext.Session.Clear();
 
-            return View();
+            return View(orderHeader);
         }
 
         public IActionResult Remove(int id)
